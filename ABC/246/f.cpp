a85a92f5be25280fe396bc3fff13814ce6320cd8
hirakuuuu <hiraku@LAPTOP-U73D98T8.localdomain>
2022-04-03 18:29:18 +0900

    20220403

--- ABC/246/f.cpp
+++ ABC/246/f.cpp
@@ -10,9 +10,42 @@ const int mod = 998244353;
 // 問題
 // https://atcoder.jp/contests/abc246/tasks/abc246_f
 
+ll binpower(ll a, ll b){
+    ll ans = 1;
+    while(b > 0){
+        if(b%2) ans = (ans*a)%mod;
+        a = (a*a)%mod;
+        b /= 2;
+    }
+    return ans;
+}
+
 int main(){
-    int n, l; cin >> n >> l;
-    
-    
+    ll n, l; cin >> n >> l;
+    ll ans = 0;
+    vector<vector<int>> s(n, vector<int>(26));
+    rep(i, 0, n){
+        string S; cin >> S;
+        for(auto ss: S) s[i][ss-'a']++;
+    }
+
+    rep(i, 1, (1<<n)){
+        vector<int> group(26, 1);
+        int b = 0;
+        rep(j, 0, n){
+            if(i&(1<<j)){
+                b++;
+                rep(k, 0, 26){
+                    group[k] &= s[j][k];
+                }
+            }
+        }
+        ll cnt = 0;
+        rep(j, 0, 26) cnt += group[j];
+        if(b%2) ans = (ans+binpower(cnt, l))%mod;
+        else ans = (ans-binpower(cnt, l))%mod;
+    }
+    if(ans < 0) ans += mod;
+    cout << ans << endl;
     return 0;
 }
