677fdde895e367f1af577bd1c9d9a1041e248220
hirakuuuu <hiraku@LAPTOP-U73D98T8.localdomain>
2022-04-07 14:25:24 +0900

    Bellman–Ford

--- ABC/137/e.cpp
+++ ABC/137/e.cpp
@@ -11,25 +11,31 @@ const int mod = 998244353;
 // https://atcoder.jp/contests/abc137/tasks/abc137_e
 
 int n, m, p;
-vector<vector<int>> g(2505, vector<int>(2505, -1));
-void dfs(int pos, int cnt){
-    rep(i, 1, n+1){
-        
+vector<vector<int>> e;
+vector<ll> a(5005), b(5005), c(5005), d(2505, -(1LL<<60));
+
+void bf(){
+    d[0] = 0;
+    rep(i, 0, n+n){
+        rep(j, 0, m){
+            if(d[a[j]] != -1LL<<60 and d[a[j]]+c[j] > d[b[j]]){
+                d[b[j]] = d[a[j]]+c[j];
+                if (i >= n-1) d[b[j]] = 1LL<<60;
+            }
+        }
     }
-
-    
-
 }
 
 int main(){
     cin >> n >> m >> p;
     rep(i, 0, m){
-        int a, b, c; cin >> a >> b >> c;
-        g[a][b] = c-p;
+        cin >> a[i] >> b[i] >> c[i];
+        a[i]--;
+        b[i]--;
+        c[i] -= p;
     }
-
-    
-
-    
+    bf();
+    if(d[n-1] >= 1LL<<60) cout << -1 << endl;
+    else cout << max(0LL, d[n-1]) << endl;
     return 0;
 }
