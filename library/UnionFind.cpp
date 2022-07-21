259982af50f9408eeaac74a0648df97abefd09a1
hirakuuuu <m.hiraku1129@gmail.com>
2022-07-21 15:12:18 +0900

    ライブラリいじり

---
+++ library/UnionFind.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define ll long long
+
+
+// ここから
+class UnionFind {
+    vector<ll> parent, maxi, mini;
+    inline ll root(ll n){
+        return (parent[n] < 0? n:parent[n] = root(parent[n]));
+    }
+public:
+    UnionFind(ll n_ = 1): parent(n_, -1), maxi(n_), mini(n_){
+        iota(maxi.begin(), maxi.end(), 0);
+        iota(mini.begin(), mini.end(), 0);
+    }
+
+    inline bool same(ll x, ll y){
+        return root(x) == root(y);
+    }
+
+    inline void unite(ll x, ll y){
+        ll rx = root(x);
+        ll ry = root(y);
+        if(rx == ry) return;
+        if(parent[rx] > parent[ry]) swap(rx, ry);
+        parent[rx] += parent[ry];
+        parent[ry] = rx;
+        maxi[x] = std::max(maxi[x],maxi[y]);
+        mini[x] = std::min(mini[x],mini[y]);
+    }
+
+    inline ll min(ll x){
+        return mini[root(x)];
+    }
+
+    inline ll max(int x){
+        return mini[root(x)];
+    }
+
+    inline ll size(ll x){
+        return (-parent[root(x)]);
+    }
+
+    inline ll operator[](ll x){
+        return root(x);
+    }
+
+    inline void print(){
+        rep(i, 0, (ll)parent.size()) cout << root(i) << " ";
+        cout << endl;
+    }
+
+    void clear(){
+        rep(i, 0, (ll)parent.size()){
+            parent[i] = -1;
+        }
+        iota(maxi.begin(), maxi.end(), 0);
+        iota(mini.begin(), mini.end(), 0);
+    }
+};
+// ここまで
+
+int main(){
+    ll n = 10;
+    UnionFind uf(n);
+
+    uf.unite(0, 1);
+    uf.unite(1, 2);
+    cout << uf.size(0) << endl;
+    cout << uf.size(3) << endl;
+    cout << uf[2] << endl;
+    uf.print();
+}
