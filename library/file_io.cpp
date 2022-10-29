3df8998586b4ed51c5990eb138734a3a50581a26
hirakuuuu <m.hiraku1129@gmail.com>
2022-10-30 00:49:54 +0900

    ファイルからの入出力

---
+++ library/file_io.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+const int MOD = 1000000007;
+const int mod = 998244353;
+
+int main(){
+    string infilename = "input.txt";
+    string outfilename = "output.txt";
+    ifstream reading_file(infilename);
+    ofstream writing_file(outfilename);
+
+    
+    
+    return 0;
+}
