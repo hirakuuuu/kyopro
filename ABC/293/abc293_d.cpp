3a89e4c504a263cc329a1dbab67a465166402859
hirakuuuu <m.hiraku1129@gmail.com>
2023-06-26 12:37:29 +0900

    [add] 20230626

---
+++ ABC/293/abc293_d.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define rrep(i, a, n) for(int i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+// constexpr ll MOD = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+
+// 問題
+// https://atcoder.jp/contests/abc293/tasks/abc293_d
+
+class UnionFind {
+    vector<ll> parent, maxi, mini;
+    inline ll root(ll n){
+        return (parent[n] < 0? n:parent[n] = root(parent[n]));
+    }
+public:
+    UnionFind(ll n_ = 1): parent(n_, -1), maxi(n_), mini(n_){
+        iota(maxi.begin(), maxi.end(), 0);
+        iota(mini.begin(), mini.end(), 0);
+    }
+
+    inline bool same(ll x, ll y){
+        return root(x) == root(y);
+    }
+
+    inline void unite(ll x, ll y){
+        ll rx = root(x);
+        ll ry = root(y);
+        if(rx == ry) return;
+        if(parent[rx] > parent[ry]) swap(rx, ry);
+        parent[rx] += parent[ry];
+        parent[ry] = rx;
+        maxi[x] = std::max(maxi[x],maxi[y]);
+        mini[x] = std::min(mini[x],mini[y]);
+    }
+
+    inline ll min(ll x){
+        return mini[root(x)];
+    }
+
+    inline ll max(int x){
+        return mini[root(x)];
+    }
+
+    inline ll size(ll x){
+        return (-parent[root(x)]);
+    }
+
+    inline ll operator[](ll x){
+        return root(x);
+    }
+
+    inline void print(){
+        rep(i, 0, (ll)parent.size()) cout << root(i) << " ";
+        cout << endl;
+    }
+
+    void clear(){
+        rep(i, 0, (ll)parent.size()){
+            parent[i] = -1;
+        }
+        iota(maxi.begin(), maxi.end(), 0);
+        iota(mini.begin(), mini.end(), 0);
+    }
+};
+
+int main(){
+    int n, m; cin >> n >> m;
+    UnionFind uf(n);
+    vector<int> A(m);
+    rep(i, 0, m){
+        int a, c;
+        char b, d;
+        cin >> a >> b >> c >> d;
+        a--; c--;
+        uf.unite(a, c);
+        A[i] = a;
+    }
+
+    set<ll> root;
+    rep(i, 0, n){
+        root.insert(uf[i]);
+    }
+
+    vector<int> cnt(n);
+    rep(i, 0, m){
+        cnt[uf[A[i]]]++;
+    }
+
+    int ans1 = 0, ans2 = 0;
+    for(auto r: root){
+        if(uf.size(r) == cnt[r]) ans1++;
+        else ans2++;
+    }
+
+    cout << ans1 << ' ' << ans2 << endl;
+
+
+
+
+    
+    return 0;
+}
