3c55f30e784256000e20d4ff1d70f9990731bca4
hirakuuuu <m.hiraku1129@gmail.com>
2023-07-31 11:00:24 +0900

    [add] 20230731

---
+++ ABC/205/abc205_e.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define rrep(i, a, n) for(int i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll MOD = 1000000007;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+
+// 問題
+// https://atcoder.jp/contests/abc205/tasks/abc205_e
+
+template <ll MOD> class modint {
+    ll val;
+    static vector<modint<MOD>> factorial_vec;
+public:
+    // コンストラクタ
+    modint(ll x = 0){
+        val = x % MOD;
+        if(val < 0) x += MOD;
+    }
+
+    // 入出力ストリーム
+    friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+        ll y; is >> y;
+        x = y;
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+        return os << x.val;
+    }
+
+    // 算術演算子
+    modint<MOD> operator -(){return modint<MOD>(-val);}
+    modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+    modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+    modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+    modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+    // 代入演算子
+    modint<MOD> &operator +=(const modint<MOD> &r){
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    modint<MOD> &operator -=(const modint<MOD> &r){
+        if(val < r.val) val += MOD;
+        val -= r.val;
+        return *this;
+    }
+    modint<MOD> &operator *=(const modint<MOD> &r){
+        val = val*r.val%MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    modint<MOD> &operator /=(const modint<MOD> &r){
+        *this *= inv(r);
+        return *this;
+    }
+
+    //等価比較演算子
+    bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+    bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+    bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+    bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+    bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+    bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+    // 累乗
+    static modint<MOD> modpow(modint<MOD> num, ll exp){
+        if(!exp) return modint<MOD>(1); // 0乗
+        modint<MOD> ret(1);
+        modint<MOD> tmp = num;
+        while(exp){
+            if(exp&1) ret *= tmp;
+            tmp *= tmp;
+            exp >>= 1;
+        }
+        return ret;
+    }
+
+    // 逆元
+    static modint<MOD> inv(modint<MOD> num){
+        return modpow(num, MOD-2);
+    }
+
+    // 階乗
+    static modint<MOD> factorial(ll n){
+        return factorial_vec[n];
+    }
+
+    // 階乗の初期化
+    static void init_factorial(ll n){
+        factorial_vec.resize(n+1);
+        factorial_vec[0] = factorial_vec[1] = 1;
+        for(int i = 2; i <= n; i++){
+            modint<MOD> ret(i);
+            factorial_vec[i] = factorial_vec[i-1]*ret;
+        }
+    }
+
+    // コンビネーション
+    static modint<MOD> combination(ll n, ll r){
+        return factorial(n) / factorial(r) / factorial(n-r);
+    }
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+int main(){
+    ll n, m, k; cin >> n >> m >> k;
+    mint::init_factorial(n+m);
+
+    mint ans = mint::combination(n+m, n);
+    if(n > m+k) ans = 0;
+    else if(n >= k+1) ans -= mint::combination(n+m, m+k+1);
+
+  
+    cout << ans << endl;
+    
+    return 0;
+}
