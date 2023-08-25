3750366f259c2e67f10ded61c195ef83633d15ea
hirakuuuu <m.hiraku1129@gmail.com>
2023-08-25 22:35:19 +0900

    [add] フェンウィック木

--- ABC/306/abc306_f.cpp
+++ ABC/306/abc306_f.cpp
@@ -16,131 +16,42 @@ template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
 // 問題
 // https://atcoder.jp/contests/abc306/tasks/abc306_f
 
-template <class T, T (*op)(T, T), T (*e)()> 
-class SegmentTree {
-    int _n, size, log;
-    vector<T> d;
-    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+template <class T>
+class FenwickTree {
+
+private:
+    int _n;
+    vector<T> data;
+
+    T sum(int r) {
+        T s = 0;
+        while(r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+    
 public:
-    SegmentTree() : SegmentTree(0) {}
-    explicit SegmentTree(int n) : SegmentTree(vector<T>(n, e())) {} // explicit で明示的に型を指定する
-    explicit SegmentTree(const vector<T> &v) : _n(int(v.size())) {
-        // sizeは_nを超える最小の2のべき乗
-        size = 1;
-        while(size < _n) size *= 2, log++; 
+    FenwickTree() : _n(0) {}
+    explicit FenwickTree(int n) : _n(n), data(n) {}
 
-        // log は木の高さ（sizeの桁数）
-        log = 0;
-        while (!(size & (1 << log))) log++;
-
-        d = vector<T>(2*size, e());
-        for(int i = 0; i < _n; i++) d[size+i] = v[i];
-        for(int i = size-1; i >= 1; i--){
-            update(i);
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while(p <= _n) {
+            data[p - 1] += x;
+            p += p & -p;
         }
     }
 
-    void set(int p, T x){
-        assert(0 <= p && p < _n);
-        p += size;
-        d[p] = x;
-        for(int i = 1; i <= log; i++) update(p >> i);
-    }
-
-    void add(int p, T x){
-        assert(0 <= p && p < _n);
-        p += size;
-        d[p] += x;
-        for(int i = 1; i <= log; i++) update(p >> i);
-    }
-
-    T get(int p) const {
-        assert(0 <= p && p < _n);
-        return d[p+size];
-    }
-
-    T prod(int l, int r) const {
+    T sum(int l, int r) {
         assert(0 <= l && l <= r && r <= _n);
-        T sml = e(), smr = e();
-        l += size;
-        r += size;
-
-        while(l < r){
-            if(l&1) sml = op(sml, d[l++]);
-            if(r&1) smr = op(d[--r], smr);
-            l >>= 1;
-            r >>= 1;
-        }
-
-        return op(sml, smr);
+        return sum(r) - sum(l);
     }
 
-    T all_prod() const {return d[1]; };
-
-    // f(op(a[l], a[l + 1], ..., a[r - 1])) = trueとなる最大のｒ
-    template <bool (*f)(T)> int max_right(int l) const {
-        return max_right(l, [](T x) { return f(x); });
-    }
-    template <class F> int max_right(int l, F f) const {
-        assert(0 <= l && l <= _n);
-        assert(f(e()));
-        if (l == _n) return _n;
-        l += size;
-        T sm = e();
-        do {
-            while (l % 2 == 0) l >>= 1;
-            if (!f(op(sm, d[l]))) {
-                while (l < size) {
-                    l = (2 * l);
-                    if (f(op(sm, d[l]))) {
-                        sm = op(sm, d[l]);
-                        l++;
-                    }
-                }
-                return l - size;
-            }
-            sm = op(sm, d[l]);
-            l++;
-        } while ((l & -l) != l);
-        return _n;
-    }
-
-    // f(op(a[l], a[l + 1], ..., a[r - 1])) = true となる最小の l
-    template <bool (*f)(T)> int min_left(int r) const {
-        return min_left(r, [](T x) { return f(x); });
-    }
-    template <class F> int min_left(int r, F f) const {
-        assert(0 <= r && r <= _n);
-        assert(f(e()));
-        if (r == 0) return 0;
-        r += size;
-        T sm = e();
-        do {
-            r--;
-            while (r > 1 && (r % 2)) r >>= 1;
-            if (!f(op(d[r], sm))) {
-                while (r < size) {
-                    r = (2 * r + 1);
-                    if (f(op(d[r], sm))) {
-                        sm = op(d[r], sm);
-                        r--;
-                    }
-                }
-                return r + 1 - size;
-            }
-            sm = op(d[r], sm);
-        } while ((r & -r) != r);
-        return 0;
-    }
 };
 
-int op(int a, int b) {
-    return a+b;
-}
-
-int e() {
-    return 0;
-}
 
 int main(){
     ll n, m; cin >> n >> m;
@@ -161,12 +72,12 @@ int main(){
 
     ll ans = 0;
     vector<int> c(n);
-    SegmentTree<int, op, e> st(c);
+    FenwickTree<int> ft(n);
     rep(k, 0, n*m){
         ll tmp = b[k];
         const auto [i, j] = id[tmp];
-        ans += (j+1)*(n-i-1)+(st.prod(i+1, n));
-        st.add(i, 1);
+        ans += (j+1)*(n-i-1)+(ft.sum(i+1, n));
+        ft.add(i, 1);
     }
     cout << ans << endl;
 
---
+++ library/tree/フェニック木.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+template <class T>
+class FenwickTree {
+
+private:
+    int _n;
+    vector<T> data;
+
+    T sum(int r) {
+        T s = 0;
+        while(r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+    
+public:
+    FenwickTree() : _n(0) {}
+    explicit FenwickTree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while(p <= _n) {
+            data[p - 1] += x;
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+};
+
+int main() {
+    ll n, m; cin >> n >> m;
+    FenwickTree<int> ft(n);
+    
+    return 0;
+}
