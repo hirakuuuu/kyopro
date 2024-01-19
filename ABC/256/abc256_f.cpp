4aebcfc4635b6ad50e779a8e678ea3fc3c1b0946
hirakuuuu <m.hiraku1129@gmail.com>
2024-01-19 22:48:44 +0900

    [add] 全方位木DP

---
+++ ABC/256/abc256_f.cpp
@@ -0,0 +1,298 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define rrep(i, a, n) for(int i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+// constexpr ll MOD = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+
+// 問題
+// https://atcoder.jp/contests/abc256/tasks/abc256_f
+
+template <ll MOD> class modint {
+    ll val;
+    static vector<modint<MOD>> factorial_vec;
+public:
+    // コンストラクタ
+    modint(ll x = 0){
+        val = x % MOD;
+        if(val < 0) x += MOD;
+    }
+
+    // 入出力ストリーム
+    friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+        ll y; is >> y;
+        x = y;
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+        return os << x.val;
+    }
+
+    // 算術演算子
+    modint<MOD> operator -(){return modint<MOD>(-val);}
+    modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+    modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+    modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+    modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+    // 代入演算子
+    modint<MOD> &operator +=(const modint<MOD> &r){
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    modint<MOD> &operator -=(const modint<MOD> &r){
+        if(val < r.val) val += MOD;
+        val -= r.val;
+        return *this;
+    }
+    modint<MOD> &operator *=(const modint<MOD> &r){
+        val = val*r.val%MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    modint<MOD> &operator /=(const modint<MOD> &r){
+        *this *= inv(r);
+        return *this;
+    }
+
+    //等価比較演算子
+    bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+    bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+    bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+    bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+    bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+    bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+    // 累乗
+    static modint<MOD> modpow(modint<MOD> num, ll exp){
+        if(!exp) return modint<MOD>(1); // 0乗
+        modint<MOD> ret(1);
+        modint<MOD> tmp = num;
+        while(exp){
+            if(exp&1) ret *= tmp;
+            tmp *= tmp;
+            exp >>= 1;
+        }
+        return ret;
+    }
+
+    // 逆元
+    static modint<MOD> inv(modint<MOD> num){
+        return modpow(num, MOD-2);
+    }
+
+    // 階乗
+    static modint<MOD> factorial(ll n){
+        modint<MOD> ret(1);
+        if(n == 0) return ret;
+        if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+        ret = factorial(n-1)*n;
+        factorial_vec.push_back(ret);
+        return ret;
+    }
+
+    // コンビネーション
+    static modint<MOD> combination(ll n, ll r){
+        return factorial(n) / factorial(r) / factorial(n-r);
+    }
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+const int vmax = 250005;
+mint fact[vmax],finv[vmax],invs[vmax];
+void initfact(){
+	fact[0]=1;
+	rep(i,1,vmax){
+		fact[i]=fact[i-1]*i;
+	}
+	finv[vmax-1]=mint::inv(fact[vmax-1]);
+	for(int i=vmax-2;i>=0;i--){
+		finv[i]=finv[i+1]*(i+1);
+	}
+	for(int i=vmax-1;i>=1;i--){
+		invs[i]=finv[i]*fact[i-1];
+	}
+}
+mint choose(int n,int k){
+	return n-k >= 0?fact[n]*finv[n-k]*finv[k]:0;
+}
+mint binom(int a,int b){
+	return 0<=a&&0<=b?fact[a+b]*finv[a]*finv[b]:0;
+}
+mint catalan(int n){
+	return binom(n,n)-(n-1>=0?binom(n-1,n+1):0);
+}
+
+template <class T, T (*op)(T, T), T (*e)()> 
+class SegmentTree {
+    int _n, size, log;
+    vector<T> d;
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+public:
+    SegmentTree() : SegmentTree(0) {}
+    explicit SegmentTree(int n) : SegmentTree(vector<T>(n, e())) {} // explicit で明示的に型を指定する
+    explicit SegmentTree(const vector<T> &v) : _n(int(v.size())) {
+        // sizeは_nを超える最小の2のべき乗
+        size = 1;
+        while(size < _n) size *= 2, log++; 
+
+        // log は木の高さ（sizeの桁数）
+        log = 0;
+        while (!(size & (1 << log))) log++;
+
+        d = vector<T>(2*size, e());
+        for(int i = 0; i < _n; i++) d[size+i] = v[i];
+        for(int i = size-1; i >= 1; i--){
+            update(i);
+        }
+    }
+
+    void set(int p, T x){
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for(int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    void add(int p, T x){
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] += x;
+        for(int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    T get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p+size];
+    }
+
+    T prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        T sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while(l < r){
+            if(l&1) sml = op(sml, d[l++]);
+            if(r&1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    T all_prod() const {return d[1]; };
+
+    // f(op(a[l], a[l + 1], ..., a[r - 1])) = trueとなる最大のｒ
+    template <bool (*f)(T)> int max_right(int l) const {
+        return max_right(l, [](T x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        T sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    // f(op(a[l], a[l + 1], ..., a[r - 1])) = true となる最小の l
+    template <bool (*f)(T)> int min_left(int r) const {
+        return min_left(r, [](T x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        T sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+};
+
+mint op(mint a, mint b) {
+    return a+b;
+}
+
+mint e() {
+    return 0;
+}
+
+
+int main(){
+    int n, q; cin >> n >> q;
+    vector<mint> a(n), ia(n), iia(n);
+    rep(i, 0, n){
+        cin >> a[i];
+        ia[i] = a[i]*(i+1);
+        iia[i] = ia[i]*(i+1);
+    }
+
+    SegmentTree<mint, op, e> st_a(a), st_ia(ia), st_iia(iia);
+
+    while(q--){
+        int t; cin >> t;
+        if(t == 1){
+            int x;
+            mint y; cin >> x >> y;
+            x--;
+            mint mx = x+1;
+            st_a.set(x, y);
+            st_ia.set(x, mx*y);
+            st_iia.set(x, mx*mx*y);
+        }else if(t == 2){
+            int x; cin >> x;
+            mint mx = x;
+            mint ans = st_iia.prod(0, x);
+            ans -= (mx*2+3)*st_ia.prod(0, x);
+            ans += (mx*mx+mx*3+2)*st_a.prod(0, x);
+            ans /= 2;
+            cout << ans << endl;
+        }
+    }
+    
+    return 0;
+}
---
+++ others/edpc/dp_v.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define rrep(i, a, n) for(int i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+// constexpr ll MOD = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+
+// 問題
+// https://atcoder.jp/contests/dp/tasks/dp_v
+
+template <class E, class V, E (*merge)(E, E), E (*e)(), E (*put_edge)(V, int), V (*put_vertex)(E, int)>
+class RerootingDP
+{
+    int n, root, inner_edge_id;
+    vector<E> outs;
+    struct edge {
+        int to, idx, xdi;
+    };
+    vector<edge> es;
+    vector<int> start;
+
+    int outs_start(int v){
+        int res = start[v]-v;
+        if(root < v) res++;
+        return res;
+    }
+
+    void es_build(){
+        vector<edge> nes(2*n-2);
+        vector<int> nstart(n+2, 0);
+        for(int i = 0; i < 2*n-2; i++){
+            nstart[start[i]+2]++;
+        }
+        for(int i = 0; i < n; i++){
+            nstart[i+1] += nstart[i];
+        }
+        for(int i = 0; i < 2*n-2; i++){
+            nes[nstart[start[i]+1]++] = es[i];
+        }
+        swap(es, nes);
+        swap(start, nstart);
+    }
+    
+public:
+    RerootingDP(int n_ = 0) : n(n_), inner_edge_id(0) {
+        es.resize(2*n-2);
+        start.resize(2*n-2);
+        if(n == 1) es_build();
+    }
+
+    void add_edge(int u, int v, int idx, int xdi){ // u→v, v→uの有向辺をそれぞれ追加
+        start[inner_edge_id] = u;
+        es[inner_edge_id] = {v, idx, xdi};
+        inner_edge_id++;
+        start[inner_edge_id] = v;
+        es[inner_edge_id] = {u, xdi, idx};
+        inner_edge_id++;
+        if(inner_edge_id == 2*n-2){
+            es_build();
+        }
+    }
+
+    vector<V> build(int root_ = 0){
+        root = root_;
+        vector<V> subdp(n); subdp[0] = put_vertex(e(), 0);
+        outs.resize(n);
+        vector<int> geta(n+1, 0);
+        for(int i = 0; i < n; i++){
+            geta[i+1] = start[i+1]-start[i]-1;
+        }
+        geta[root+1]++;
+        for(int i = 0; i < n; i++){
+            geta[i+1] += geta[i];
+        }
+
+        auto dfs = [&](auto sfs, int v, int f)-> void {
+            E val = e();
+            for(int i = start[v]; i < start[v+1]; i++){
+                if(es[i].to == f){
+                    swap(es[start[v+1]-1], es[i]);
+                }
+                if(es[i].to == f) continue;
+                sfs(sfs, es[i].to, v);
+                E nval = put_edge(subdp[es[i].to], es[i].idx);
+                outs[geta[v]++] = nval;
+                val = merge(val, nval);
+            }
+            subdp[v] = put_vertex(val, v);
+        };
+        dfs(dfs, root, -1);
+        return subdp;
+    }
+
+    vector<V> reroot(){
+        vector<E> reverse_edge(n);
+        reverse_edge[root] = e();
+        vector<V> answers(n);
+        auto dfs = [&](auto sfs, int v) -> void {
+            int le = outs_start(v);
+            int ri = outs_start(v+1);
+            int siz = ri-le;
+            vector<E> rui(siz+1);
+            rui[siz] = e();
+            for(int i = siz-1; i >= 0; i--){
+                rui[i] = merge(outs[le+i], rui[i+1]);
+            }
+            answers[v] = put_vertex(merge(rui[0], reverse_edge[v]), v);
+            E lui = e();
+            for(int i = 0; i < siz; i++){
+                V rdp = put_vertex(merge(merge(lui, rui[i+1]), reverse_edge[v]), v);
+                reverse_edge[es[start[v]+i].to] = put_edge(rdp, es[start[v]+i].xdi);
+                lui = merge(lui, outs[le+i]);
+                sfs(sfs, es[start[v]+i].to);
+            }
+        };
+        dfs(dfs, root);
+        return answers;
+    }
+};
+
+using E = ll;
+using V = ll;
+ll m;
+E merge(E a, E b){
+    return (a * b)%m;
+}
+E e(){
+    return E(1);
+}
+E put_edge(V v, int i){
+    return (v + 1)%m;
+}
+V put_vertex(E e, int v){
+    return e%m;
+}
+
+
+int main(){
+    ll n; cin >> n >> m;
+    RerootingDP<E, V, merge, e, put_edge, put_vertex> rdp(n);
+    rep(i, 0, n-1){
+        int x, y; cin >> x >> y;
+        x--, y--;
+        rdp.add_edge(x, y, i, i);
+    }
+
+    rdp.build();
+    vector<V> ans = rdp.reroot();
+    rep(i, 0, n){
+        cout << ans[i] << endl;
+    }
+    
+    return 0;
+}
