5b43bcffe4e36fee865d5058e6499d8f95b3242d
hirakuuuu <m.hiraku1129@gmail.com>
2024-04-27 20:56:43 +0900

    [del] template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
+// #include <atcoder/all>
 using namespace std;
-using namespace atcoder;
+// using namespace atcoder;
 #define rep(i, a, n) for(int i = a; i < n; i++)
 #define rrep(i, a, n) for(int i = a; i >= n; i--)
 #define inr(l, x, r) (l <= x && x < r)
@@ -9,7 +9,7 @@ using namespace atcoder;
 #define ld long double
 
 // using mint = modint1000000007;
-using mint = modint998244353;
+// using mint = modint998244353;
 constexpr int IINF = 1001001001;
 constexpr ll INF = 9e18;
 
